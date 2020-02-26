ff74f80d3db2f97ca83b1c5df6804b82bb186bce
Benjamin Smith <smith-b9@ulster.ac.uk>
2020-02-26 19:55:51 +0000

    Updated bluetooth code. Still not functional for connecting to a device from the list

--- app/src/main/java/com/example/demoapp/ui/bluetooth/MyBluetoothService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demoapp.ui.bluetooth
-
-import java.util.*
-
-class MyBluetoothService {
-    private val NAME = "MyBTService"
-    private val MY_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-}
