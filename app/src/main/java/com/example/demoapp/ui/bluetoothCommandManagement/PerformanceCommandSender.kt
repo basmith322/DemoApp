973620eb5bd4d1b57dd8a0c97f6d0bfd6b54e8a7
basmi <smith-b9@ulster.ac.uk>
2020-03-31 18:50:06 +0100

    Changed Performance Command to run selectProtocolCommand auto to try and obtain the correct OBD protocol before sending commands

--- app/src/main/java/com/example/demoapp/ui/bluetoothCommandManagement/PerformanceCommandSender.kt
+++ app/src/main/java/com/example/demoapp/ui/bluetoothCommandManagement/PerformanceCommandSender.kt
@@ -1,10 +1,14 @@
 package com.example.demoapp.ui.bluetoothCommandManagement
 
 import android.bluetooth.BluetoothDevice
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.example.demoapp.ui.performance.PerformanceViewModel
 import com.github.pires.obd.commands.SpeedCommand
 import com.github.pires.obd.commands.engine.RPMCommand
 import com.github.pires.obd.commands.pressure.BarometricPressureCommand
+import com.github.pires.obd.commands.protocol.SelectProtocolCommand
+import com.github.pires.obd.enums.ObdProtocols
 import java.io.InputStream
 import java.io.OutputStream
 
@@ -12,6 +16,13 @@ class PerformanceCommandSender(device: BluetoothDevice, providedViewModel: Perfo
     AbstractCommandSender<PerformanceViewModel>(device, providedViewModel) {
 
     override fun performCommand(inputStream: InputStream, outputStream: OutputStream) {
+        val selectProtocolCommand = SelectProtocolCommand(ObdProtocols.AUTO)
+        try {
+            selectProtocolCommand.run(inputStream,outputStream)
+        }catch (e: Exception) {
+            Log.e(TAG, "Error connecting protocol", e)
+        }
+
         val speedCommand = SpeedCommand()
         speedCommand.run(inputStream, outputStream)
         val speedResult = speedCommand.imperialSpeed.toInt()
